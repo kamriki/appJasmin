199ee51001d30653700846504aaeb852e6a9e5c3
Khaled Amriki <kamriki@gmail.com>
2020-11-20 20:10:22 +0300

    0.0.2 geolocation api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appJasmin';
+  latitude: number;
+  longitude: number;
+
+  /**
+   * get device geolocation
+   * ensure geolocation permission is requested on iPhone - ToDo
+   */
+  ngOnInit() {
+    if (!navigator.geolocation) { console.log('location is not supported'); }
+    else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+  }
 }
