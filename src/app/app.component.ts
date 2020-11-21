d9dee2c62f4076bc02f1fd092559ed7c0df3d87d
Khaled Amriki <kamriki@gmail.com>
2020-11-21 14:43:34 +0300

    0.0.7 map convert coordinates to pixels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,19 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'appJasmin';
-  latitude: number;
-  longitude: number;
-  //private canvas: HTMLCanvasElement;
-  // private ctx: CanvasRenderingContext2D;
-  private circle = { x: 100, y: 100, size: 20, dx: 3, dy: 2 };
 
   @ViewChild('canvas', { static: true })
   protected canvas: ElementRef;
   private ctx: CanvasRenderingContext2D;
+  private mapWidth =  window.innerWidth * 0.9;
+  private mapHeight = window.innerHeight * 0.7;
+  private virusWidth = 10;
+  public player: { x: number, y: number } = { x: null, y: null }; //!! set as private
 
   /**
    * get or watch device geolocation
    * ensure geolocation permission is requested on iPhone - ToDo
+   * prepare Arena (playing Area) by setting width & hight with respect to the browser width/lenght
    */
   ngOnInit() {
     if (!navigator.geolocation) { console.log('location is not supported'); }
@@ -28,90 +28,78 @@ export class AppComponent implements OnInit {
       //   this.latitude = position.coords.latitude;
       //   this.longitude = position.coords.longitude;
       // });
-      // this.watchPosition();
+      this.watchPosition();
     }
 
+    this.canvas.nativeElement.width = this.mapWidth;
+    this.canvas.nativeElement.height = this.mapHeight;
+
     this.ctx = this.canvas.nativeElement.getContext('2d');
-    // this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    // this.ctx = this.canvas.getContext("2d");
-
-    //this.drawCircle();
-    this.update();
-
-    // var x = 0;
-    // setInterval(() => {
-    //   this.ctx.clearRect(0, 0, 200, 200);
-    //   this.drawCircle(x % 200);
-    //   // this.drawBorder(x % 200);
-    //   x++;
-    // }, 25);
+    // this.sendVirus(100, 10);
   }
 
   /**
-   *
+   * to specify direction - ToDo
    */
-  drawCircle() {
+  sendVirus(posY: number, speed: number) {
+    var x = 0;
+    setInterval(() => {
+      this.ctx.clearRect(0, 0, this.mapWidth, this.mapHeight);
+      this.creatVirus(x % this.mapWidth + this.virusWidth, posY);
+      x++;
+    }, speed);
+  }
+
+  /**
+   * drawing of viruse
+   */
+  creatVirus(posX: number, posY: number) {
     this.ctx.beginPath();
-    this.ctx.arc(this.circle.x, this.circle.y, this.circle.size, 0, Math.PI * 2);
-    this.ctx.fillStyle = 'purple';
+    this.ctx.arc(posX, posY, this.virusWidth, 0, 2 * Math.PI);
+    this.ctx.fillStyle = "purple";
     this.ctx.fill();
   }
-  // drawCircle(x) {
-  //   this.ctx.beginPath();
-  //   this.ctx.arc(x, 100, 10, 0, 2 * Math.PI);
-  //   this.ctx.fillStyle = "red";
-  //   this.ctx.fill();
+
+  /** */
+  // update() {
+  //   // console.log(this.ctx);
+  //   this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  //   this.drawCircle();
+
+  //   // chnage position
+  //   this.circle.x += this.circle.dx;
+  //   this.circle.y += this.circle.dy;
+
+  //   // detect side walls | top & bottom walls
+  //   if (this.circle.x + this.circle.size > this.canvas.nativeElement.width || this.circle.x - this.circle.size < 0) {
+  //     this.circle.dx *= -1;
+  //   }
+  //   if (this.circle.y + this.circle.size > this.canvas.nativeElement.height || this.circle.y - this.circle.size < 0) {
+  //     this.circle.dy *= -1;
+  //   }
+  //   window.requestAnimationFrame(this.update);
   // }
-
-  /** */
-  drawBorder(x) {
-    this.ctx.beginPath();
-    this.ctx.moveTo(0, 0);
-    this.ctx.lineTo(200, 0);
-    this.ctx.lineTo(200, 200);
-    this.ctx.lineTo(0, 200);
-    this.ctx.lineTo(0, 0);
-    this.ctx.stroke();
-  }
-
-  /** */
-  update() {
-    // console.log(this.ctx);
-    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    this.drawCircle();
-
-    // chnage position
-    this.circle.x += this.circle.dx;
-    this.circle.y += this.circle.dy;
-
-    // detect side walls | top & bottom walls
-    if (this.circle.x + this.circle.size > this.canvas.nativeElement.width || this.circle.x - this.circle.size < 0) {
-      this.circle.dx *= -1;
-    }
-    if (this.circle.y + this.circle.size > this.canvas.nativeElement.height || this.circle.y - this.circle.size < 0) {
-      this.circle.dy *= -1;
-    }
-    window.requestAnimationFrame(this.update);
-  }
 
   /**
    * update device geolocation every 5 sec
+   * save location to storage - !! to remove
    */
   watchPosition() {
     navigator.geolocation.watchPosition(
       (position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.saveLocal('coordinate', {
-          time: new Date(),
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
+        // this.player.latitude = position.coords.latitude;
+        // this.player.longitude = position.coords.longitude;
+        this.mapCoordinates(position.coords.latitude, position.coords.longitude);
+        // this.saveLocal('coordinate', {
+        //   time: new Date(),
+        //   latitude: position.coords.latitude,
+        //   longitude: position.coords.longitude
+        // });
       },
       (err) => { console.log(err); },
       {
         enableHighAccuracy: false,
-        timeout: 5000,
+        timeout: 0,
         maximumAge: 0
       }
     );
@@ -124,7 +112,7 @@ export class AppComponent implements OnInit {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
-  /**
+  /** !! NOT USED
    * get data from localStorage
    */
   getLocal(key: string) {
@@ -133,5 +121,17 @@ export class AppComponent implements OnInit {
       console.log(JSON.parse(value));
       //return JSON.parse(value)
     }
+  }
+
+  /**
+   * get x
+   * convert from degrees to radians
+   */
+  mapCoordinates(lat: number, lng: number) {
+    const locX = (lng + 180) * (this.mapWidth / 360);
+    const latRad = lat * Math.PI / 180;
+    const mercN = Math.log(Math.tan((Math.PI / 4) + (latRad / 2)));
+    const locY = (this.mapHeight / 2) - (this.mapWidth * mercN / (2 * Math.PI));
+    this.player = {x: locX, y: locY };
   }
 }
