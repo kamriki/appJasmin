b05f2eb80dbb211eb82e2c6defd9858aab7a282f
Khaled Amriki <kamriki@gmail.com>
2020-11-21 13:48:47 +0300

    0.0.6 test canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,13 @@ export class AppComponent implements OnInit {
   title = 'appJasmin';
   latitude: number;
   longitude: number;
+  //private canvas: HTMLCanvasElement;
+  // private ctx: CanvasRenderingContext2D;
+  private circle = { x: 100, y: 100, size: 20, dx: 3, dy: 2 };
+
+  @ViewChild('canvas', { static: true })
+  protected canvas: ElementRef;
+  private ctx: CanvasRenderingContext2D;
 
   /**
    * get or watch device geolocation
@@ -21,8 +28,70 @@ export class AppComponent implements OnInit {
       //   this.latitude = position.coords.latitude;
       //   this.longitude = position.coords.longitude;
       // });
-      this.watchPosition();
+      // this.watchPosition();
     }
+
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    // this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    // this.ctx = this.canvas.getContext("2d");
+
+    //this.drawCircle();
+    this.update();
+
+    // var x = 0;
+    // setInterval(() => {
+    //   this.ctx.clearRect(0, 0, 200, 200);
+    //   this.drawCircle(x % 200);
+    //   // this.drawBorder(x % 200);
+    //   x++;
+    // }, 25);
+  }
+
+  /**
+   *
+   */
+  drawCircle() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.circle.x, this.circle.y, this.circle.size, 0, Math.PI * 2);
+    this.ctx.fillStyle = 'purple';
+    this.ctx.fill();
+  }
+  // drawCircle(x) {
+  //   this.ctx.beginPath();
+  //   this.ctx.arc(x, 100, 10, 0, 2 * Math.PI);
+  //   this.ctx.fillStyle = "red";
+  //   this.ctx.fill();
+  // }
+
+  /** */
+  drawBorder(x) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 0);
+    this.ctx.lineTo(200, 0);
+    this.ctx.lineTo(200, 200);
+    this.ctx.lineTo(0, 200);
+    this.ctx.lineTo(0, 0);
+    this.ctx.stroke();
+  }
+
+  /** */
+  update() {
+    // console.log(this.ctx);
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.drawCircle();
+
+    // chnage position
+    this.circle.x += this.circle.dx;
+    this.circle.y += this.circle.dy;
+
+    // detect side walls | top & bottom walls
+    if (this.circle.x + this.circle.size > this.canvas.nativeElement.width || this.circle.x - this.circle.size < 0) {
+      this.circle.dx *= -1;
+    }
+    if (this.circle.y + this.circle.size > this.canvas.nativeElement.height || this.circle.y - this.circle.size < 0) {
+      this.circle.dy *= -1;
+    }
+    window.requestAnimationFrame(this.update);
   }
 
   /**
